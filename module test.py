491a90bcbf13a33b6cd2e34783451086f5b9e33f
Gurralachandana <chandanagurrala0@gmail.com>
2025-01-09 11:56:14 +0530

    Add files via upload

---
+++ module test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import compute
+
+
+# In[2]:
+
+
+compute.mean_value(20,30,40,50)
+
+
+# In[3]:
+
+
+compute.product(50*30*40*10)
+
+
+# In[ ]:
+
+
+
+
